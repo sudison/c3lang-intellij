5847abf2388edc9f97c357fb480bec5310a35ef2
GitHub Action <action@github.com>
2022-01-05 23:31:06 +0000

    Template cleanup

--- src/main/kotlin/com/github/sudison/c3langintellij/MyBundle.kt
+++ src/main/kotlin/com/github/sudison/c3langintellij/MyBundle.kt
@@ -1,4 +1,4 @@
-package org.jetbrains.plugins.template
+package com.github.sudison.c3langintellij
 
 import com.intellij.DynamicBundle
 import org.jetbrains.annotations.NonNls
--- src/main/kotlin/com/github/sudison/c3langintellij/listeners/MyProjectManagerListener.kt
+++ src/main/kotlin/com/github/sudison/c3langintellij/listeners/MyProjectManagerListener.kt
@@ -1,9 +1,9 @@
-package org.jetbrains.plugins.template.listeners
+package com.github.sudison.c3langintellij.listeners
 
 import com.intellij.openapi.components.service
 import com.intellij.openapi.project.Project
 import com.intellij.openapi.project.ProjectManagerListener
-import org.jetbrains.plugins.template.services.MyProjectService
+import com.github.sudison.c3langintellij.services.MyProjectService
 
 internal class MyProjectManagerListener : ProjectManagerListener {
 
