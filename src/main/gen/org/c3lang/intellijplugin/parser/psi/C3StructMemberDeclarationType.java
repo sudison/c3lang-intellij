6d02f175454db948e0e987e8c792114fdac54908
Disheng Su <sudison@gmail.com>
2022-03-06 01:23:09 -0800

    missing files

---
+++ src/main/gen/org/c3lang/intellijplugin/parser/psi/C3StructMemberDeclarationType.java
@@ -0,0 +1,16 @@
+// This is a generated file. Not intended for manual editing.
+package org.c3lang.intellijplugin.parser.psi;
+
+import java.util.List;
+import org.jetbrains.annotations.*;
+import com.intellij.psi.PsiElement;
+
+public interface C3StructMemberDeclarationType extends PsiElement {
+
+  @Nullable
+  C3IdentifierList getIdentifierList();
+
+  @NotNull
+  C3Type getType();
+
+}
